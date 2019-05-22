0cc78702f75da51b4342f09cf180396bf269e347
Yonnick <yonnick00@gmail.com>
2019-05-22 18:35:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,5 +17,31 @@ class App < Sinatra::Base
     "#{@phrase * @number}"
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation].to_s
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+
+    if @operation == "add"
+      @sum = @num1 + @num2
+    elsif @operation == "subtract"
+      @sum = @num1 - @num2
+    elsif @operation == "multiply"
+      @sum = @num1 * @num2
+    elsif @operation == "divide"
+      @sum = @num1 / @num2
+    end
+    "#{@sum}"
+  end
+
 
 end
